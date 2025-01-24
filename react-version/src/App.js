2024167db9e346813f7e82fb54e015cb0f88314d
semblant <robynhumphreys7_4@hotmail.com>
2025-01-23 22:07:40 -0700

    Create functions for components, add components by calling the functions, add separate html files for each component

---
+++ react-version/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './components/Navigation';
+import Profile from './components/Profile';
+import TweetForm from './components/TweetForm';
+import TweetList from './components/TweetList';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      <Profile/>
+      <main className="container">
+        <TweetForm/>
+        <TweetList/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
